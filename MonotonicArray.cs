12b680c60fcfd2b63aa271d003c0ef089905a85d
Samuel Baraka <sababuvercetti@gmail.com>
2022-10-17 09:27:04 +0300

    MOnotonic array solution

---
+++ MonotonicArray.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class MonotonicProgram {
+	public static bool IsMonotonic(int[] array) {
+		if(array.Count()<=1){
+            return true;
+        }
+        int pointerIndex=1;
+        for(int i=0;i<array.Count();i++){
+            while(pointerIndex<array.Count()-1){
+            if(array[po])
+            }
+
+        }
+	}
+}
