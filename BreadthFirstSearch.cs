ce40a27e3df43d30648660a0906674fee8c73288
Samuel Baraka <sababuvercetti@gmail.com>
2022-10-24 16:39:37 +0300

    Fibonacci solution

--- BreadthFirstSearch.cs
+++ BreadthFirstSearch.cs
@@ -8,8 +8,32 @@ public class BreadthFirstProgram {
 	// and methods to the class.
 	public class Node {
 
-        public static void Main(string[] args)
+        public static void DMain(string[] args)
         {
+			Node node = new Node("A");
+			node.children = new List<Node>();
+			node.children.Add(new Node("B"));
+			node.children.Add(new Node("C"));
+			node.children.Add(new Node("D"));
+			node.children[0].children = new List<Node>();
+			node.children[0].children.Add(new Node("E"));
+			node.children[0].children.Add(new Node("F"));
+			node.children[2].children = new List<Node>();
+			node.children[2].children.Add(new Node("G"));
+			node.children[2].children.Add(new Node("H"));
+			node.children[2].children.Add(new Node("I"));
+			node.children[0].children[1].children = new List<Node>();
+			node.children[0].children[1].children.Add(new Node("J"));
+			node.children[0].children[1].children.Add(new Node("K"));
+			node.children[2].children[2].children = new List<Node>();
+			node.children[2].children[2].children.Add(new Node("L"));
+			node.children[2].children[2].children.Add(new Node("M"));
+			List<string> result = node.BreadthFirstSearch(new List<string>());
+			foreach (string a in result)
+			{
+				Console.WriteLine(a + " ");
+			}
+
 
         }
 		public string name;
---
+++ Fibonacci.cs
@@ -0,0 +1,63 @@
+using System;
+
+public class FibonacciProgram {
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(GetNthFib(6));
+    }
+    // O(2^n) time | O(n) space
+    // This is a recursive solution/ Brute force solution
+
+	// public static int GetNthFib(int n) {
+    //     // Write your code here.
+    //     if(n==1){
+    //         return 0;
+    //     }else if(n==2){
+    //         return 1;
+    //     }else{
+    //         return GetNthFib(n-1)+GetNthFib(n-2);
+    //     }
+		
+	// }
+
+    // O(n) time | O(n) space
+    // This is a recursive solution with memoization
+   
+    // public static int GetNthFib(int n, Dictionary<int, int>? memoize = null) {
+    //     if(memoize==null){
+    //         memoize=new Dictionary<int, int>(){
+    //             {1,0},
+    //             {2,1}
+    //         };
+    //     }
+
+    //     if(memoize.ContainsKey(n)){
+    //         return memoize[n];
+    //     }else{
+    //         if(n==1){
+    //             return 0;
+    //         }else if(n==2){
+    //             return 1;
+    //         }else{
+    //             memoize[n]=GetNthFib(n-1,memoize)+GetNthFib(n-2,memoize);
+    //             return memoize[n];
+    //         }
+    //     }
+    // }
+
+
+    // O(n) time | O(1) space
+    public static int GetNthFib(int n) {
+        var lastTwo = new int[] {0,1};
+        int counter = 3;
+        while(counter<=n){
+            int nextFib = lastTwo[0]+lastTwo[1];
+            lastTwo[0]=lastTwo[1];
+            lastTwo[1]=nextFib;
+            counter++;
+        }
+        return n>1?lastTwo[1]:lastTwo[0];
+
+    }
+}
