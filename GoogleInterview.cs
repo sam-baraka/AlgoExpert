7695e597e07bafce94154f1b3ff3d4a141b00a0e
Samuel Baraka <sababuvercetti@gmail.com>
2022-10-25 14:05:45 +0300

    my google interview solution (failed in the interview, but did it after the interview)

---
+++ GoogleInterview.cs
@@ -0,0 +1,32 @@
+class GoogleInterviewProgram{
+    static void Main(string[] args){
+        Console.WriteLine(Calculate());
+    }
+
+    /// Calculate value from string
+    static int Calculate(){
+        /// The answer should be 55
+        /// Using a queue
+        String toBeCalculated = "add(5,mul(2,pow(5,2)))";
+        Queue<String> queue = new Queue<String>();
+        String[] split = toBeCalculated.Split(new char[]{'(',')',','});
+        foreach(String s in split){
+            queue.Enqueue(s);
+        }
+        return CalculateValue(queue);
+
+    }
+
+    static int CalculateValue(Queue<String> queue){
+        String operation = queue.Dequeue();
+        if(operation.Equals("add")){
+            return CalculateValue(queue)+CalculateValue(queue);
+        }else if(operation.Equals("mul")){
+            return CalculateValue(queue)*CalculateValue(queue);
+        }else if(operation.Equals("pow")){
+            return (int)Math.Pow(CalculateValue(queue),CalculateValue(queue));
+        }else{
+            return int.Parse(operation);
+        }
+    }
+}
