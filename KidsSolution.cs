92314f714dde2aa2fa5551b43dbab0ba7375294d
Samuel Baraka <sababuvercetti@gmail.com>
2023-03-08 22:23:41 +0300

    kids solution

---
+++ KidsSolution.cs
@@ -0,0 +1,22 @@
+class KidsSolution{
+    public static void Main(string[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int sum = 0;
+        for(int i = 0; i < arr.Length; i++){
+            sum += arr[i];
+        }
+        
+    }
+     int getSolution(int n,int k,int s){
+        var arr= new int[n];
+        for(int i = s; i < n+s; i++){
+            if(i<n){
+                
+            }else{
+
+            }
+        }
+
+     return 0;
+     }
+}
--- NonConstructibleChange.cs
+++ NonConstructibleChange.cs
@@ -13,6 +13,6 @@ public class NonConstructibleProgram {
             currentChangeCreated += coin;
         }
         return currentChangeCreated + 1;
-		
+
 	}
 }
